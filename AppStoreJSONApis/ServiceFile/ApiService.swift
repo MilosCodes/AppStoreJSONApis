f083b5a7ec275d2ad92a23e4dfb75542f46de453
MilosCodes <milos.codes@gmail.com>
2020-08-13 23:18:49 +0200

    made groups fetching
    made groups fetching for AppsPageController and API fixed

--- AppStoreJSONApis/ServiceFile/ApiService.swift
+++ AppStoreJSONApis/ServiceFile/ApiService.swift
@@ -11,58 +11,63 @@ import Foundation
 
 class Service {
     
-    static let shared = Service() // Singleton
+    static let shared = Service() // singleton
     
-    func fetchApps(searchTerm: String, completion: @escaping ([Result], Error?) -> ())  {
+    func fetchApps(searchTerm: String, completion: @escaping ([Result], Error?) -> ()) {
+        print("Fetching itunes apps from Service layer")
+        
         let urlString = "https://itunes.apple.com/search?term=\(searchTerm)&entity=software"
-        
         guard let url = URL(string: urlString) else { return }
-        // fetch data from internet
         
+        // fetch data from internet
         URLSession.shared.dataTask(with: url) { (data, resp, err) in
-            //if error fetch
-            
             if let err = err {
-                print("failed to fetch apps", err)
+                print("Failed to fetch apps:", err)
                 completion([], nil)
+                return
             }
-            guard let data = data else { return }
             
+            // success
+            guard let data = data else { return }
             do {
                 let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+                
                 completion(searchResult.results, nil)
                 
-                
-            }
-            catch let jsonErr {
-                print("Failed to decode JSONN", jsonErr)
+            } catch let jsonErr {
+                print("Failed to decode json:", jsonErr)
                 completion([], jsonErr)
             }
             
-            
-        }.resume() //fires off the request
+            }.resume() // fires off the request
+    }
+    
+    func fetchTopGrossing(completion: @escaping (AppGroup?, Error?) -> ()) {
+        let urlString = "https://rss.itunes.apple.com/api/v1/us/ios-apps/top-grossing/all/50/explicit.json"
+        fetchAppGroup(urlString: urlString, completion: completion)
     }
     
     func fetchGames(completion: @escaping (AppGroup?, Error?) -> ()) {
-        guard let url = URL(string: "https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/50/explicit.json") else { return }
+        fetchAppGroup(urlString: "https://rss.itunes.apple.com/api/v1/us/ios-apps/new-games-we-love/all/50/explicit.json", completion: completion)
+    }
+    
+    //helper
+    func fetchAppGroup(urlString: String, completion: @escaping (AppGroup?, Error?) -> Void) {
+        guard let url = URL(string: urlString) else { return }
         
         URLSession.shared.dataTask(with: url) { (data, resp, err) in
-            
             if let err = err {
                 completion(nil, err)
                 return
-                
             }
             do {
-                
                 let appGroup = try JSONDecoder().decode(AppGroup.self, from: data!)
-
-                //success case
-                appGroup.feed.results.forEach({print($0.name)})
+                // success
                 completion(appGroup, nil)
-            } catch let jsonAppGroupErr {
-                completion(nil, jsonAppGroupErr)
+            } catch {
+                completion(nil, error)
             }
-        }.resume()
+            }.resume()
     }
+    
 }
