c09a4a0fe1349d5f73ec1544a72ac635ccdb6858
MilosCodes <milos.codes@gmail.com>
2020-08-20 21:58:35 +0200

    AppRowCell, AppCell
    AppRowCell, AppCell

---
+++ AppStoreJSONApis/Controller/details/ReviewRowCell.swift
@@ -0,0 +1,33 @@
+//
+//  ReviewRowCell.swift
+//  AppStoreJSONApis
+//
+//  Created by Milos Milivojevic on 20/08/2020.
+//  Copyright © 2020 Milos Milivojevic. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+import UIKit
+
+class ReviewRowCell: UICollectionViewCell {
+    
+    let reviewsController = ReviewsController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .yellow
+        
+        addSubview(reviewsController.view)
+        reviewsController.view.fillSuperview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+}
