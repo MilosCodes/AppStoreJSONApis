9dcbce9e1ac075b787065832871b318c52a534d1
MilosCodes <milos.codes@gmail.com>
2020-08-20 08:45:44 +0200

    dammit
    dammit

--- AppStoreJSONApis/Controller/details/PreviewCell.swift
+++ AppStoreJSONApis/Controller/details/PreviewCell.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class PreviewCell: UICollectionViewCell {
     
+    let previewLabel = UILabel(text: "Preview", font: .boldSystemFont(ofSize: 20), numberOfLines: 1)
+    let horizontalController = PreviewScreenshotsController()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .red
+        backgroundColor = .init(white: 0.95, alpha: 0.8)
+        
+        addSubview(previewLabel)
+        previewLabel.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(10)
+            make.left.equalToSuperview().offset(16)
+        }
+        
+        addSubview(horizontalController.view)
+        horizontalController.view.snp.makeConstraints { (make) in
+            make.top.lessThanOrEqualTo(previewLabel.snp.bottom).offset(5)
+            make.left.bottom.right.equalToSuperview()
+        }
     }
     
     
---
+++ AppStoreJSONApis/Controller/details/PreviewScreenshotsController.swift
@@ -0,0 +1,67 @@
+//
+//  PreviewScreenshotsController.swift
+//  AppStoreJSONApis
+//
+//  Created by Milos Milivojevic on 19/08/2020.
+//  Copyright © 2020 Milos Milivojevic. All rights reserved.
+//
+
+import UIKit
+
+
+
+class PreviewScreenshotsController: HorizontalSnappingController, UICollectionViewDelegateFlowLayout {
+    
+    
+    let cellId = "cellId"
+    
+    var app: Result? {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .white
+        collectionView.register(ScreenshotCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.contentInset = .init(top: 0, left: 16, bottom: 0, right: 16)
+    
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { return app?.screenshotUrls.count ?? 0 }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! ScreenshotCell
+        
+        let screenshotUrl = self.app?.screenshotUrls[indexPath.item]
+        cell.imageView.sd_setImage(with: URL(string: screenshotUrl ?? ""))
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 250, height: view.frame.height)
+    }
+}
+
+
+class ScreenshotCell: UICollectionViewCell {
+
+    let imageView = UIImageView(cornerRadius: 12)
+
+      
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(imageView)
+        
+        imageView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
